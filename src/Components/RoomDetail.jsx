49cb90987182b7ef38ae6778e23bd372b7b2a102
amirho3in <amirhoseinhaerian@gmail.com>
2024-03-28 11:52:06 +0100

    fetching data from API this time for getting each room with relevand Persons

--- src/Components/RoomDetail.jsx
+++ src/Components/RoomDetail.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const RoomDetail = () => {
-  
+  const { id } = useParams();
+  const [room, setRoom] = useState(null);
 
-  return <></>;
+  useEffect(() => {
+    const fetchRoom = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_PATH}room/${id}`, {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            api_key: import.meta.env.VITE_API_KEY,
+          },
+        });
+        const data = await response.json();
+        console.log(data);
+        setRoom(data);
+      } catch (error) {
+        console.error("Error fetching room details:", error);
+      }
+    };
+
+    fetchRoom();
+  }, [id]);
+
+  if (!room) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>{room.name}</h2>
+        <p>{room.isBooked ? `The room is booked by:` : "The room is Available"}</p>
+      </div>
+      <ul>
+        {room.relation.map((obj) => (
+          <li key={obj.person._id}>
+            {obj.person.firstName} {obj.person.lastName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RoomDetail;
--- src/Components/RoomList.jsx
+++ src/Components/RoomList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const RoomList = () => {
   const [rooms, setRooms] = useState([]);
@@ -19,7 +19,6 @@ const RoomList = () => {
 
     fetchRooms();
   }, []);
-
 
   return (
     <div>
