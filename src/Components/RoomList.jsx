aca12d34091d23ec78ba88626825a8093c7050dd
amirho3in <amirhoseinhaerian@gmail.com>
2024-03-28 11:44:48 +0100

    Refactor: fetching data from API is working now

--- src/Components/RoomList.jsx
+++ src/Components/RoomList.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const RoomList = () => {
-  
+  const [rooms, setRooms] = useState([]);
 
-  return <></>;
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const response = await fetch(`${import.meta.env.VITE_API_PATH}rooms`, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          api_key: import.meta.env.VITE_API_KEY,
+        },
+      });
+      const data = await response.json();
+      setRooms(data);
+    };
+
+    fetchRooms();
+  }, []);
+
+
+  return (
+    <div>
+      <h2>All the Rooms</h2>
+      {rooms.map((room) => (
+        <div key={room._id}>
+          <Link to={`/room/${room._id}`}>
+            <span>{room.name}</span>
+            <span>{room.isBooked ? "Booked" : "Available"}</span>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default RoomDetail;
+export default RoomList;
