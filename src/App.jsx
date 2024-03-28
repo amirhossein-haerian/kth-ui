c78048e731f7cdd9ee49799c1450287581cbb4d5
amirho3in <amirhoseinhaerian@gmail.com>
2024-03-28 11:39:46 +0100

    Refactor: Project Structure is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoomList from "./Components/RoomList";
+import RoomDetail from './Components/RoomDetail';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<RoomList />} />
+        <Route path="/room/:id" element={<RoomDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
